8b67584e3cc3d9029d45dc68272b7b2140968502
suropark <xoopp5818@gmail.com>
2021-02-04 20:29:12 +0900

    form onsubmit 이벤트가 button에서 갑자기 작동안함, button --> input type="submit"으로 변경

--- hufsweb_client/src/index.js
+++ hufsweb_client/src/index.js
@@ -2,11 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+
+
+
+
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+
+<App />
+,
   document.getElementById('root')
 );
 
