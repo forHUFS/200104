76bb59de3a8bb23fd9f308781c306867d42684bc
suropark <xoopp5818@gmail.com>
2021-02-04 20:29:24 +0900

    Revert "form onsubmit 이벤트가 button에서 갑자기 작동안함, button --> input type="submit"으로 변경"
    This reverts commit 8b67584e3cc3d9029d45dc68272b7b2140968502.

--- hufsweb_client/src/_actions/user_action.js
+++
@@ -1,6 +0,0 @@
-
-
-export function registerUser(data){
-   console.log("user_action");
-    // const request = axios.post('/api/users/register', data).then(console.log())
-}
--- hufsweb_client/src/index.js
+++ hufsweb_client/src/index.js
@@ -2,17 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import { Provider } from 'react-redux';
-import { createStore } from 'redux';
-
-
-
-
 
 ReactDOM.render(
-
-<App />
-,
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
   document.getElementById('root')
 );
 
