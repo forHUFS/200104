2de2b594bf1fc93bb5d946a8da582be7d2e9c8ab
suropark <xoopp5818@gmail.com>
2021-02-04 20:30:34 +0900

    Revert "Revert "form onsubmit 이벤트가 button에서 갑자기 작동안함, button --> input type="submit"으로 변경""
    This reverts commit 76bb59de3a8bb23fd9f308781c306867d42684bc.

---
+++ hufsweb_client/src/_actions/user_action.js
@@ -0,0 +1,6 @@
+
+
+export function registerUser(data){
+   console.log("user_action");
+    // const request = axios.post('/api/users/register', data).then(console.log())
+}
--- hufsweb_client/src/components/views/RegisterPage/RegisterPage.js
+++ hufsweb_client/src/components/views/RegisterPage/RegisterPage.js
@@ -1,137 +1,90 @@
-import React from 'react'
+import React, { useState } from "react";
+import { useDispatch } from "react-redux";
+import { registerUser } from "../../../_actions/user_action";
+import "./RegisterPage.css";
+function RegisterPage(props) {
+  const [Id, setId] = useState("");
+  const [Password, setPassword] = useState("");
+  const [ConfirmPassword, setConfirmPassword] = useState("");
+  const [Email, setEmail] = useState("");
 
-import "./RegisterPage.css"
-function RegisterPage() {
-    return (
-        <>
-        <div id="header">
-            <a href="https://nid.naver.com/user2/V2Join.nhn?m=agree#agreeBottom" target="_blank" title="네이버 회원가입 페이지 보러가기">이미지</a>
-        </div>
+  // handler 만들기
+  const onIdHandler = (event) => {
+    setId(event.currentTarget.value);
+  };
 
+  const onPasswordHandler = (event) => {
+    setPassword(event.currentTarget.value);
+  };
+  const onConfirmPasswordHandler = (event) => {
+    setConfirmPassword(event.currentTarget.value);
+  };
+  const onEmailHandler = (event) => {
+    setEmail(event.currentTarget.value);
+  };
+  const onSubmitHandler = (event) => {
+    console.log("submit");
+    event.preventDefault();
+                                 //정규식으로 아이디 비밀번호 이메일 검증 필요
+    if (Password !== ConfirmPassword) {
+      return alert("비밀번호 재확인");
+    }
 
-        <div id="wrapper">
+    let body = {
+      id: Id,
+      password: Password,
+      email: Email,
+    };
 
-            <div id="content">
+    registerUser(body); // to action
+  };
 
-                <div>
-                    <h3 className="join_title">
-                        <label for="id">아이디</label>
-                    </h3>
-                    <span className="box int_id">
-                        <input type="text" id="id" className="int" maxlength="20"/>
-                        <span className="step_url">@naver.com</span>
-                    </span>
-                    <span className="error_next_box"></span>
-                </div>
+  return (
+    <>
+      <div className="header">헤더</div>
+      <div className="content">
+        <form onSubmit={onSubmitHandler}>
+          <div>
+            <h3>
+              <label>아이디</label>
+            </h3>
+            <input type="text" value={Id} onChange={onIdHandler} />
+          </div>
 
-                <div>
-                    <h3 className="join_title"><label for="pswd1">비밀번호</label></h3>
-                    <span className="box int_pass">
-                        <input type="text" id="pswd1" className="int" maxlength="20"/>
-                        <span id="alertTxt">사용불가</span>
-이미지                    </span>
-                    <span className="error_next_box"></span>
-                </div>
+          <div>
+            <h3 className="join_title">
+              <label>비밀번호</label>
+            </h3>
+            <input
+              type="password"
+              value={Password}
+              onChange={onPasswordHandler}
+            />
+          </div>
 
-                <div>
-                    <h3 className="join_title"><label for="pswd2">비밀번호 재확인</label></h3>
-                    <span className="box int_pass_check">
-                        <input type="text" id="pswd2" className="int" maxlength="20"/>
-                        <img src="m_icon_check_disable.png" id="pswd2_img1" className="pswdImg"/>
-                    </span>
-                    <span className="error_next_box"></span>
-                </div>
+          <div>
+            <h3 className="join_title">
+              <label>비밀번호 재확인</label>
+            </h3>
+            <input
+              type="password"
+              value={ConfirmPassword}
+              onChange={onConfirmPasswordHandler}
+            />
+          </div>
 
-                <div>
-                    <h3 className="join_title"><label for="name">이름</label></h3>
-                    <span className="box int_name">
-                        <input type="text" id="name" className="int" maxlength="20"/>
-                    </span>
-                    <span className="error_next_box"></span>
-                </div>
-
-                <div>
-                    <h3 className="join_title"><label for="yy">생년월일</label></h3>
-
-                    <div id="bir_wrap">
-                        <div id="bir_yy">
-                            <span className="box">
-                                <input type="text" id="yy" className="int" maxlength="4" placeholder="년(4자)"/>
-                            </span>
-                        </div>
-
-                        <div id="bir_mm">
-                            <span className="box">
-                                <select id="mm" className="sel">
-                                    <option>월</option>
-                                    <option value="01">1</option>
-                                    <option value="02">2</option>
-                                    <option value="03">3</option>
-                                    <option value="04">4</option>
-                                    <option value="05">5</option>
-                                    <option value="06">6</option>
-                                    <option value="07">7</option>
-                                    <option value="08">8</option>
-                                    <option value="09">9</option>                                    
-                                    <option value="10">10</option>
-                                    <option value="11">11</option>
-                                    <option value="12">12</option>
-                                </select>
-                            </span>
-                        </div>
-
-                        <div id="bir_dd">
-                            <span className="box">
-                                <input type="text" id="dd" className="int" maxlength="2" placeholder="일"/>
-                            </span>
-                        </div>
-
-                    </div>
-                    <span className="error_next_box"></span>    
-                </div>
-
-                <div>
-                    <h3 className="join_title"><label for="gender">성별</label></h3>
-                    <span className="box gender_code">
-                        <select id="gender" className="sel">
-                            <option>성별</option>
-                            <option value="M">남자</option>
-                            <option value="F">여자</option>
-                        </select>                            
-                    </span>
-                    <span className="error_next_box">필수 정보입니다.</span>
-                </div>
-
-                <div>
-                    <h3 className="join_title"><label for="email">본인확인 이메일<span className="optional">(선택)</span></label></h3>
-                    <span className="box int_email">
-                        <input type="text" id="email" className="int" maxlength="100" placeholder="선택입력"/>
-                    </span>
-                    <span className="error_next_box">이메일 주소를 다시 확인해주세요.</span>    
-                </div>
-
-                <div>
-                    <h3 className="join_title"><label for="phoneNo">휴대전화</label></h3>
-                    <span className="box int_mobile">
-                        <input type="tel" id="mobile" className="int" maxlength="16" placeholder="전화번호 입력"/>
-                    </span>
-                    <span className="error_next_box"></span>    
-                </div>
-
-
-                <div className="btn_area">
-                    <button type="button" id="btnJoin">
-                        <span>가입하기</span>
-                    </button>
-                </div>
-
-                
-
-            </div> 
-
-        </div> 
-        </>
-    )
+          <div>
+            <h3 className="join_title">
+              <label>본인확인 이메일</label>
+            </h3>
+            <input type="email" value={Email} onChange={onEmailHandler} />
+          </div>
+          <br />
+          <input type="submit" value="가입" />
+        </form>
+      </div>
+    </>
+  );
 }
 
-export default RegisterPage
+export default RegisterPage;
